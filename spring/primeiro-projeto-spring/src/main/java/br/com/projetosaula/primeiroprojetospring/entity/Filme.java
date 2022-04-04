1549657296fc116ea8e16d49d9d21a0aed392c7e
Alessandro Ferreira <94122439+alessandrobferreira@users.noreply.github.com>
2022-04-04 18:41:19 -0300

    Add files via upload

---
+++ spring/primeiro-projeto-spring/src/main/java/br/com/projetosaula/primeiroprojetospring/entity/Filme.java
@@ -0,0 +1,75 @@
+package br.com.projetosaula.primeiroprojetospring.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Filme {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column
+	private String titulo;
+	
+	@ManyToOne
+	private Genero genero;
+	
+	@ManyToOne
+	private Restricao restricao;
+	
+	@Column
+	private int favorito;
+	
+
+	public Filme() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public Restricao getRestricao() {
+		return restricao;
+	}
+
+	public void setRestricao(Restricao restricao) {
+		this.restricao = restricao;;
+	}
+
+	public int getFavorito() {
+		return favorito;
+	}
+
+	public void setFavorito(int favorito) {
+		this.favorito = favorito;
+	}
+
+}
---
+++ spring/primeiro-projeto-spring/target/classes/data.sql
@@ -0,0 +1,19 @@
+/*INSERT INTO GENERO(descricao) VALUES('Aventura');
+INSERT INTO GENERO(descricao) VALUES('Fantasia');
+INSERT INTO GENERO(descricao) VALUES('Ficção');
+INSERT INTO GENERO(descricao) VALUES('Drama');
+INSERT INTO GENERO(descricao) VALUES('Ação');
+
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('LIVRE', 0);
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('10 anos', 10);
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('12 anos', 12);
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('14 anos', 14);
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('16 anos', 16);
+INSERT INTO RESTRICAO(descricao, idade) VALUES ('18 anos', 18);
+*/
+
+
+
+
+INSERT INTO FILME(titulo, genero_id, restricao_id, favorito) value('Ready Player One', 3, 3, 1);
+INSERT INTO FILME(titulo, genero_id, restricao_id, favorito) value('Harry Potter', 2, 1, 1);
